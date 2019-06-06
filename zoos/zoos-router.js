8c48ceb06e3946415148133160e436f551ed4eff
James Pak <jamesjiminpak@gmail.com>
2019-06-06 14:10:42 -0700

    update

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -60,10 +60,10 @@ router.put("/:id", (req, res) => {
     // .where({ id: req.params.id })
     // .update(changes)
     .then(count => {
-        if (count > 0) {
-            res.status(200).json({ message: `${count} records updated` })
+        if (count) {
+            res.status(200).json(count)
         } else {
-            res.status(404).json({ message: 'Zoo not found!'})
+            res.status(404).json({ message: `Zoo ${count} not found!`})
         }
     })
     .catch(error => {
